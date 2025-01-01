a462f3d3b8f6a8ca0ac5e71b9fba9c820b77a7ac
BYUNGUI KANG <kevin180@naver.com>
2025-01-01 09:38:06 +0700

    getpostbyslug

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,68 +1,35 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import { getPostBySlug } from '@/lib/posts';
+import { Metadata } from 'next';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
-import { formatDate } from '@/lib/utils'
-import MDXContent from '@/components/mdx-content'
-import { getPosts, getPostBySlug } from '@/lib/posts'
-import { ArrowLeftIcon } from '@radix-ui/react-icons'
-import { notFound } from 'next/navigation'
-import NewsletterForm from '@/components/newsletter-form'
-
-export async function generateStaticParams() {
-  const posts = await getPosts()
-  const slugs = posts.map(post => ({ slug: post.slug }))
-
-  return slugs
+interface PageProps {
+  params: {
+    slug: string;
+  };
 }
 
-export default async function Post({ params }: { params: { slug: string } }) {
-  const { slug } = params
-  const post = await getPostBySlug(slug)
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const post = await getPostBySlug(params.slug);
+
+  return {
+    title: post?.metadata.title,
+    description: post?.metadata.summary,
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const post = await getPostBySlug(params.slug);
 
   if (!post) {
-    notFound()
+    return <div>Post not found</div>;
   }
 
-  const { metadata, content } = post
-  const { title, image, author, publishedAt } = metadata
-
   return (
-    <section className='pb-24 pt-32'>
-      <div className='container max-w-3xl'>
-        <Link
-          href='/posts'
-          className='mb-8 inline-flex items-center gap-2 text-sm font-light text-muted-foreground transition-colors hover:text-foreground'
-        >
-          <ArrowLeftIcon className='h-5 w-5' />
-          <span>Back to posts</span>
-        </Link>
-
-        {image && (
-          <div className='relative mb-6 h-96 w-full overflow-hidden rounded-lg'>
-            <Image
-              src={image}
-              alt={title || ''}
-              className='object-cover'
-              fill
-            />
-          </div>
-        )}
-
-        <header>
-          <h1 className='title'>{title}</h1>
-          <p className='mt-3 text-xs text-muted-foreground'>
-            {author} / {formatDate(publishedAt ?? '')}
-          </p>
-        </header>
-
-        <main className='prose mt-16 dark:prose-invert'>
-          <MDXContent source={content} />
-        </main>
-
-        <footer className='mt-16'>
-          <NewsletterForm />
-        </footer>
-      </div>
-    </section>
-  )
+    <article className="prose prose-neutral dark:prose-invert">
+      <h1>{post.metadata.title}</h1>
+      <MDXRemote source={post.content} />
+    </article>
+  );
 }
