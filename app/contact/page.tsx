981fd8e344b8d3969ec3f33414f94610d66a8277
BYUNGUI KANG <kevin180@naver.com>
2025-01-15 18:16:36 +0700

    contact font size bigger

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,7 @@ export default function Contact() {
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
-        <h2 className='title'>궁금하신 사항은 언제든지 문의주시면 감사하겠습니다.</h2>
-
+        <h2 className='title text-4xl'>궁금하신 사항은 언제든지 문의주시면 감사하겠습니다.</h2>
         <ContactForm />
       </div>
     </section>
