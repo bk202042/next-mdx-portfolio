715c08a4ca5950e204d3c6e667765411ef395ab8
BYUNGUI KANG <kevin180@naver.com>
2025-01-15 15:50:52 +0700

    header to korean

--- components/header.tsx
+++ components/header.tsx
@@ -7,19 +7,19 @@ export default function Header() {
       <nav className='container flex max-w-3xl items-center justify-between'>
         <div>
           <Link href='/' className='font-serif text-2xl font-bold'>
-            HB
+            BK
           </Link>
         </div>
 
-        <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10'>
+        <ul className='flex items-center gap-6 text-sm font-medium text-foreground sm:gap-10'>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/posts'>Posts</Link>
+            <Link href='/posts'>블로그</Link>
           </li>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/projects'>Projects</Link>
+            <Link href='/projects'>프로젝트</Link>
           </li>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/contact'>Contact</Link>
+            <Link href='/contact'>문의하기</Link>
           </li>
         </ul>
 
